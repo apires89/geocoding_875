eeed67dcbda5366de4c58d38f0d3719421b2c32e
andre <andrecabralpires@gmail.com>
2022-05-25 10:00:22 +0200

    geocoding lecture

---
+++ app/javascript/controllers/mapbox_controller.js
@@ -0,0 +1,55 @@
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from "mapbox-gl"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    console.log("mapbox!")
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/apires89/cjpb0eog5a2tx2spj2qhqytwz"
+    })
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }))
+  }
+
+  #addMarkersToMap() {
+  this.markersValue.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window) // add this
+    // Create a HTML element for your custom marker
+    const customMarker = document.createElement("div")
+    customMarker.className = "marker"
+    customMarker.style.backgroundImage = `url('${marker.image_url}')`
+    customMarker.style.backgroundSize = "contain"
+    customMarker.style.width = "25px"
+    customMarker.style.height = "25px"
+   new mapboxgl.Marker(customMarker)
+   .setLngLat([marker.lng,marker.lat])
+    .setPopup(popup) // add this
+   .addTo(this.map)
+  });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend( [
+      marker.lng,marker.lat
+    ]))
+    this.map.fitBounds(bounds, {
+      padding: 70, maxZoom: 20, duration: 0
+    })
+  }
+
+}
